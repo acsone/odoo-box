65d2473f23228f169e6bf912386b68d6f27bbae7
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2014-09-25 10:46:36 +0200

    [FIX] Set max-depth on find command

--- link.sh
+++ link.sh
@@ -3,6 +3,6 @@
 cd /home/vagrant
 sudo rm -rf *.sh
 cd /home/vagrant/odoo
-sudo -u vagrant find -name "*.sh" -exec ln -s odoo/{} ../{} \; 
+sudo -u vagrant find -maxdepth 1 -name "*.sh" -exec ln -s odoo/{} ../{} \; 
 cd ..
 chmod u+x *.sh
